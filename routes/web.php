816737038cb9aaf99207cb98aeb702187f79147a
GvMazzon25 <gvmazzon25@gmail.com>
2022-02-01 15:26:40 +0100

    admin

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
 
+Route::middleware('auth')
+     ->namespace('Admin')
+     ->name('admin.')
+     ->prefix('admin')
+     ->group(function(){
+         //Admin Homepage
+         Route::get('/home', 'HomeController@index')->name('home');
+
+         //Post resource routes
+     });
 
