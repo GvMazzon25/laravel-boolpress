59fed8e976f3b71ec9c7a05aa9fb20da240777e3
GvMazzon25 <gvmazzon25@gmail.com>
2022-02-13 13:10:59 +0100

    show-category-end

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
     //Pagina Categoria
 
     public function show($id){
-        return view('admin.categories.show');
+        $category = Category::find($id);
+
+        return view('admin.categories.show', compact('category'));
     }
 }
--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mb-3">Blog Film</h1>
+    <h1 class="mb-3">{{ $category->name }}</h1>
+
+    @foreach ($category->films as $film)
+      <article>
+          <h3>{{ $film->name}}</h3>
+          <a class="btn btn-success" href="{{route('admin.film.show', $film->id)}}">Show</a>
+          <a class="btn btn-danger" href="{{route('admin.film.edit', $film->id)}}">Edit</a>
+          <hr>
+      </article>
+    @endforeach
 </div>
 @endsection
